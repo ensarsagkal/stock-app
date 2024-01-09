869b93ef528cacac70ec3bc425bcc0d97c63651f
ensar <ensarsagkal@gmail.com>
2024-01-09 13:14:54 +0300

    routes fixed

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,11 +4,12 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
-import  Home  from "../pages/Home";
-import  Purchases  from "../pages/Purchases";
+import Home from "../pages/Home";
+import Purchases from "../pages/Purchases";
 import Sales from "../pages/Sales";
 import Firms from "../pages/Firms";
 import Brands from "../pages/Brands";
+import Products from "../pages/Products";
 
 const AppRouter = () => {
   return (
@@ -18,11 +19,12 @@ const AppRouter = () => {
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
           <Route path="" element={<Dashboard />}>
-          <Route index element={<Home />} />
-          <Route path="/purchases" element={<Purchases/>} />
-          <Route path="/sales" element={<Sales/>} />
-          <Route path="/firms" element={<Firms/>} />
-          <Route path="/brands" element={<Brands/>} />
+            <Route index element={<Home />} />
+            <Route path="purchases" element={<Purchases />} />
+            <Route path="sales" element={<Sales />} />
+            <Route path="firms" element={<Firms />} />
+            <Route path="brands" element={<Brands />} />
+            <Route path="products" element={<Products />} />
           </Route>
         </Route>
       </Routes>
