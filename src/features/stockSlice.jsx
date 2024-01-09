8b6c8e9c77b1e521088861fae07f872993ca690e
ensar <ensarsagkal@gmail.com>
2024-01-09 20:48:32 +0300

    slice added

---
+++ src/features/stockSlice.jsx
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  firms: [],
+  products: [],
+  purchases: [],
+  brands: [],
+  sales: [],
+  loading:false,
+  error:false,
+}
+
+const stockSlice = createSlice({
+  name: "stock",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+    },
+    firmsSuccess:(state,{payload})=>{
+      state.loading=false
+      state.firms=payload.data
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+})
+
+export const {fetchStart,fetchFail,firmsSuccess} = stockSlice.actions
+
+export default stockSlice.reducer
