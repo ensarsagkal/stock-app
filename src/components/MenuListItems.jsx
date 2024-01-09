c99ca93839336d1cd8a7d73e98cfdc124fe49635
ensar <ensarsagkal@gmail.com>
2024-01-09 13:10:27 +0300

    appRouter updated

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard"
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
+import InventoryIcon from "@mui/icons-material/Inventory"
+import StoreIcon from "@mui/icons-material/Store"
+import StarsIcon from "@mui/icons-material/Stars"
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
+import { useNavigate } from "react-router-dom"
+
+
+const icons = [
+    {
+      icon: <SpaceDashboardIcon />,
+      title: "Dashboard",
+      url: "/stock/",
+    },
+    {
+      title: "Purchases",
+      icon: <ShoppingCartIcon />,
+      url: "/stock/purchases/",
+    },
+    {
+      title: "Sales",
+      icon: <AttachMoneyIcon />,
+      url: "/stock/sales/",
+    },
+    {
+      title: "Firms",
+      icon: <StoreIcon />,
+      url: "/stock/firms/",
+    },
+    {
+      title: "Brands",
+      icon: <StarsIcon />,
+      url: "/stock/brands/",
+    },
+    {
+      title: "Products",
+      icon: <InventoryIcon />,
+      url: "/stock/products/",
+    },
+  ]
 
 const MenuListItems = () => {
+const navigate=useNavigate()
   return (
     <List >
-    {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-      <ListItem key={text} disablePadding>
+    {icons.map((item, index) => (
+      <ListItem key={index} disablePadding onClick={()=>navigate(item.url)}>
         <ListItemButton>
-          <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-          </ListItemIcon>
-          <ListItemText primary={text} />
+          <ListItemIcon sx={{color:"white"}}>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.title}  sx={{color:"white"}}/>
         </ListItemButton>
       </ListItem>
     ))}
