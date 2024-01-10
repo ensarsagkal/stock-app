6915a405d685ed6f4127fd3640989a6e3ad0e1a2
ensar <ensarsagkal@gmail.com>
2024-01-10 21:06:29 +0300

    persist upd

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice"
 import stockReducer from "../features/stockSlice"
-import { persistStore, persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage" // defaults to localStorage for web
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+// import storage from "redux-persist/lib/storage" //? local storage
+import storage from "redux-persist/lib/storage/session" //? session storage
 
 const persistConfig = {
   key: "root",
@@ -17,6 +27,12 @@ const store = configureStore({
     stock: stockReducer
   },
   devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 export const persistor = persistStore(store)
 
